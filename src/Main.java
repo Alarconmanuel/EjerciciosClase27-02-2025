f95b7b555192b79a71b9171c4bb163ae33948606
Manuel ALarcon <azaeduardo1@gmail.com>
2025-02-27 17:00:11 -0500

    Ejercicio Dado

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int min = 1;
+        int max = 10;
+        int rondas, d1, d2;
+        int contD1=0;
+        int contD2=0;
+
+        Random ale = new Random();
+
+
+        rondas = ale.nextInt((max- min + 1)) + min;
+        System.out.println("CANTIDAD DE RONDAS: " + rondas);
+
+        for (int i=1; i<= rondas; i++ ){
+            d1 = ale.nextInt((6- 1 + 1)) + min;
+            contD1 +=d1;
+            d2 = ale.nextInt((6- 1 + 1)) + min;
+            contD2 +=d2;
+
+            System.out.println();
+            System.out.println("Dado 1: " + d1);
+
+            System.out.println("Dado 2: " +d2);
+            System.out.println();
+
+        }
+
+        System.out.println("Suma Dado 1: " +contD1);
+        System.out.println("Suma Dado 2: " +contD2);
+        System.out.println();
+
+        if (contD1 > contD2) {
+            System.out.println("La suma del Dado 1 es mayor");
+        } else {
+            System.out.println("La suma del Dado 2 es mayor");
+        }
+
+
+        }
+    }
