d3d9237fc3e84a396feef750b36cf08092ebacb5
Manuel ALarcon <azaeduardo1@gmail.com>
2025-02-27 17:58:19 -0500

    Ejercicio adivina numero

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,21 +8,34 @@ public class Main {
         int min = 1;
         int max = 50;
         int num;
+        int intentos = 3;
 
 
         Random ale = new Random();
 
 
-        num = ale.nextInt((max- min + 1)) + min;
+        num = ale.nextInt((max - min + 1)) + min;
+        System.out.println("Intenta adivinar el numero de 1 a 50 en 3 intentos");
+
+        int op;
+        Scanner teclado = new Scanner(System.in);
 
 
+        for (int i = 1; i <= intentos; i++){
+            System.out.println("Ingrese un numero: ");
+            op = teclado.nextInt();
 
-        if (contD1 > contD2) {
-            System.out.println("La suma del Dado 1 es mayor");
-        } else {
-            System.out.println("La suma del Dado 2 es mayor");
+            if (op > num) {
+                System.out.println("El numero a adivinar es menor");
+            } else if (op < num) {
+                System.out.println("El numero a adivinar es mayor");
+            } else {
+                System.out.println("Felicitaciones adivinaste el numero");
+            }
+
         }
 
+        System.out.println("Perdiste el numero era: " + num);
 
-        }
     }
+}
