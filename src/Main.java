b9636aa304faf785cdd8c8526f618f4e337a0208
Manuel ALarcon <azaeduardo1@gmail.com>
2025-02-27 17:15:29 -0500

    Rama2

--- src/Main.java
+++ src/Main.java
@@ -5,34 +5,16 @@ public class Main {
 
 
         int min = 1;
-        int max = 10;
-        int rondas, d1, d2;
-        int contD1=0;
-        int contD2=0;
+        int max = 50;
+        int num;
+
 
         Random ale = new Random();
 
 
-        rondas = ale.nextInt((max- min + 1)) + min;
-        System.out.println("CANTIDAD DE RONDAS: " + rondas);
+        num = ale.nextInt((max- min + 1)) + min;
 
-        for (int i=1; i<= rondas; i++ ){
-            d1 = ale.nextInt((6- 1 + 1)) + min;
-            contD1 +=d1;
-            d2 = ale.nextInt((6- 1 + 1)) + min;
-            contD2 +=d2;
 
-            System.out.println();
-            System.out.println("Dado 1: " + d1);
-
-            System.out.println("Dado 2: " +d2);
-            System.out.println();
-
-        }
-
-        System.out.println("Suma Dado 1: " +contD1);
-        System.out.println("Suma Dado 2: " +contD2);
-        System.out.println();
 
         if (contD1 > contD2) {
             System.out.println("La suma del Dado 1 es mayor");
