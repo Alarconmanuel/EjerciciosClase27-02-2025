03b8cd798b2881423413cddc1738aeb043910811
Manuel ALarcon <azaeduardo1@gmail.com>
2025-02-28 17:45:24 -0500

    Ejercicio Apuesta

--- src/Main.java
+++ src/Main.java
@@ -6,36 +6,38 @@ public class Main {
 
 
         int min = 1;
-        int max = 50;
-        int num;
-        int intentos = 3;
+        int max = 3;
+        int num, dinero ;
+        int respuesta = 1;
 
 
         Random ale = new Random();
 
 
-        num = ale.nextInt((max - min + 1)) + min;
-        System.out.println("Intenta adivinar el numero de 1 a 50 en 3 intentos");
-
-        int op;
         Scanner teclado = new Scanner(System.in);
+        num = ale.nextInt((max - min + 1)) + min;
+        System.out.print("Ingresa la cantidad de dinero que desees apostar: ");
+        dinero = teclado.nextInt();
 
 
-        for (int i = 1; i <= intentos; i++){
-            System.out.println("Ingrese un numero: ");
-            op = teclado.nextInt();
-
-            if (op > num) {
-                System.out.println("El numero a adivinar es menor");
-            } else if (op < num) {
-                System.out.println("El numero a adivinar es mayor");
+        do {
+            if (num==1) {
+                dinero *= 2;
+                System.out.println("Duplicaste tu dinero");
+                System.out.println("Si Quieres seguir jugando oprime 1 de lo contario 2");
+                respuesta = teclado.nextInt();
+            } else if (num==2) {
+                dinero /= 2;
+                System.out.println("Perdiste la mitad de tu dinero");
+                System.out.println("Si Quieres seguir jugando oprime 1 de lo contario 2");
+                respuesta = teclado.nextInt();
             } else {
-                System.out.println("Felicitaciones adivinaste el numero");
+                dinero *=0;
+                System.out.println("Perdiste tu dinero");
             }
-
+        } while (respuesta==1);
+         System.out.println("Te retiraste del juego tu dinero actual es: " +dinero);
         }
 
-        System.out.println("Perdiste el numero era: " + num);
-
     }
-}
+
