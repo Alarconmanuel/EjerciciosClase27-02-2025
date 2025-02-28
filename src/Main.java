58e7eb227a3c6deee4aee78187f039f804418834
Manuel ALarcon <azaeduardo1@gmail.com>
2025-02-28 16:45:57 -0500

    Rama3

--- src/Main.java
+++ src/Main.java
@@ -4,38 +4,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
-        int min = 1;
-        int max = 50;
-        int num;
-        int intentos = 3;
-
-
-        Random ale = new Random();
-
-
-        num = ale.nextInt((max - min + 1)) + min;
-        System.out.println("Intenta adivinar el numero de 1 a 50 en 3 intentos");
-
-        int op;
         Scanner teclado = new Scanner(System.in);
 
+        System.out.println("Ingrese el peso de la persona en Kg");
+        double peso = teclado.nextDouble();
 
-        for (int i = 1; i <= intentos; i++){
-            System.out.println("Ingrese un numero: ");
-            op = teclado.nextInt();
+        System.out.println("Ingrese la estatura de la persona en metros");
+        double estatura = teclado.nextDouble();
 
-            if (op > num) {
-                System.out.println("El numero a adivinar es menor");
-            } else if (op < num) {
-                System.out.println("El numero a adivinar es mayor");
-            } else {
-                System.out.println("Felicitaciones adivinaste el numero");
-            }
 
-        }
 
-        System.out.println("Perdiste el numero era: " + num);
+        System.out.println("Perdiste el numero era: " );
 
     }
 }
