9e00aa88ef9472b8b55db5cfac76abf995ca1530
Manuel ALarcon <azaeduardo1@gmail.com>
2025-02-28 18:11:56 -0500

    Ejercicio IMC

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -9,23 +8,32 @@ public class Main {
 
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("Ingrese el peso de la persona en Kg");
+        System.out.print("Ingrese el peso de la persona en Kg: ");
         peso = teclado.nextDouble();
 
-        System.out.println("Ingrese la estatura de la persona en metros");
+        System.out.print("Ingrese la estatura de la persona en metros: ");
         estatura = teclado.nextDouble();
         imc = peso/(estatura * estatura);
         System.out.println("Tu IMC es: "+imc);
 
         if (imc < 18.5){
             System.out.println("Bajo peso");
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            System.out.println("peso normal");
+        } else if (imc >= 25 && imc <=26.9 ) {
+            System.out.println("Sobrepeso grado I");
+        } else if (imc >= 27 && imc <=29.9){
+            System.out.println("Sobrepeso grado II");
+        } else if (imc >= 30 && imc <=34.9 ) {
+            System.out.println("Obesidad tipo I");
+        } else if (imc >= 35 && imc <=39.9 ) {
+            System.out.println("Obesidad tipo II");
+        } else if (imc >= 40 && imc <=49.9 ) {
+            System.out.println("Obesidad tipo III (mórbida)");
+        } else {
+            System.out.println("Obesidad tipo IV (extrema)");
         }
 
-
-
-
-
-        System.out.println("Perdiste el numero era: " );
 
     }
 }
